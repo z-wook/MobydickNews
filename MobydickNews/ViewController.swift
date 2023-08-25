d5f9dde8e4483ff9bb014e8b2a1c2568e9fbb65e
jiwook_han <jiwook.han.dev@gmail.com>
2023-08-25 10:52:13 +0900

    init project

---
+++ MobydickNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobydickNews
+//
+//  Copyright (c) 2023 z-wook. All right reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
