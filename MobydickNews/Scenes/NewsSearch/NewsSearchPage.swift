ee33759ea86b7aa3fd9c494c258adb1d49ff6a27
Luna828 <eskw0701@naver.com>
2023-08-27 01:15:56 +0900

    [FEAT]: 하드코딩으로 공통 cell 작업하기

--- MobydickNews/Scenes/NewsSearch/NewsSearchPage.swift
+++ MobydickNews/Scenes/NewsSearch/NewsSearchPage.swift
@@ -6,7 +6,7 @@ final class NewsSearchPage: UIViewController {
     let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.backgroundImage = UIImage() // Search바 기본구성인 두가지의 선을 지우는 ㄱ
-        searchBar.placeholder = "검색"
+        searchBar.placeholder = "검색할 단어를 입력해주세요"
         return searchBar
     }()
         
@@ -18,14 +18,12 @@ final class NewsSearchPage: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         /**
          할일
          1. 테이블 뷰 셀 (공통 부분 만들기)
          2. 서치바 만들기
          3.  NewsApiDataManager을 사용하여
          **/
-        
         setupUI()
         
         // 테이블 뷰의 dataSource와 delegate를 설정
@@ -33,7 +31,7 @@ final class NewsSearchPage: UIViewController {
         searchTableView.delegate = self
         
         // Cell 등록
-        searchTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        searchTableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
     // UI 요소를 설정하는 메서드
@@ -42,7 +40,7 @@ final class NewsSearchPage: UIViewController {
            view.addSubview(searchTableView)
            
            searchBar.snp.makeConstraints { make in
-               make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+               make.top.equalTo(view.safeAreaLayoutGuide).offset(CGFloat.defaultPadding)
                make.leading.equalToSuperview().offset(16)
                make.trailing.equalToSuperview().offset(-16)
            }
@@ -61,8 +59,10 @@ extension NewsSearchPage: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Row \(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+//        cell.imageView?.image = UIImage(systemName: "person")
+//        cell.textLabel?.text = "Row \(indexPath.row)"
         return cell
     }
 }
