9d1be05d335e4ac7adce9cde8cc22ba41469be7c
Luna828 <eskw0701@naver.com>
2023-08-29 16:29:43 +0900

    [FEAT]: Combine 사용하여 MVVM 나누고 비동기 처리하기

--- MobydickNews/Scenes/NewsSearch/NewsSearchPage.swift
+++ MobydickNews/Scenes/NewsSearch/NewsSearchPage.swift
@@ -1,9 +1,12 @@
 import SnapKit
+import Combine
 import UIKit
 
 final class NewsSearchPage: UIViewController {
+    private let viewModel = NewsSearchViewModel()
+    private var cancelable = Set<AnyCancellable>()
     // 검색 바
-    let searchBar: UISearchBar = {
+    private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.backgroundImage = UIImage() // Search바 기본구성인 두가지의 선을 지우는 ㄱ
         searchBar.placeholder = "검색할 단어를 입력해주세요"
@@ -11,29 +14,31 @@ final class NewsSearchPage: UIViewController {
     }()
         
     // 검색 결과를 보여줄 테이블 뷰
-    let searchTableView: UITableView = {
+    private lazy var searchTableView: UITableView = {
         let tableView = UITableView()
+        
         return tableView
     }()
     
-    var data: NewsData?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
-        
-        // 테이블 뷰의 dataSource와 delegate를 설정
         searchTableView.dataSource = self
         searchTableView.delegate = self
-        
         // Cell 등록
         searchTableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
-        
-        NewsApiDataManager.shared.getSearchNews(searchTitle: "apple") { [weak self] newsData in
-            guard let self = self else { return }
-            self.data = newsData
-            self.searchTableView.reloadData()
-        }
+        viewModel.getAllHeadLineNews()
+        setupUI()
+        bindViewModel()
+    }
+    
+    func bindViewModel() {
+        viewModel.$newsList.receive(on: DispatchQueue.main).sink { [weak self] newsList in
+            guard let self = self,
+                  let articles = newsList?.articles else { return }
+            if !articles.isEmpty {
+                self.searchTableView.reloadData()
+            }
+        }.store(in: &cancelable)
     }
     
     // UI 요소를 설정하는 메서드
@@ -58,22 +63,19 @@ final class NewsSearchPage: UIViewController {
 
 extension NewsSearchPage: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let data = data,
-              let articles = data.articles else {return 0}
+        guard let articles = viewModel.newsList?.articles else {return 0}
         return articles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
         
-        guard let data = data,
-              let articles = data.articles else { return UITableViewCell() }
+        guard let articles = viewModel.newsList?.articles else { return UITableViewCell() }
         let temp = articles[indexPath.row]
         guard let title = temp.title,
               let description = temp.description,
               let date = temp.publishedAt,
               let image = temp.urlToImage else { return UITableViewCell() }
-        
         cell.configure(title: title, description: description, date: date, imageString: image)
         return cell
     }
---
+++ MobydickNews/Scenes/NewsSearch/ViewModel/NewsSearchViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  NewsSearchViewModel.swift
+//  MobydickNews
+//
+//  Created by t2023-m0050 on 2023/08/29.
+//
+
+import Combine
+import Foundation
+
+final class NewsSearchViewModel: ObservableObject {
+    private let newsManager = NewsApiDataManager.shared
+    @Published var newsList: NewsData?
+
+    func getAllHeadLineNews() {
+        newsManager.getAllHeadLineNews { [weak self] newsData in
+            guard let self = self else { return }
+            self.newsList = newsData
+        }
+    }
+
+    func getSearchNewsData(searchTitle: String) {
+        newsManager.getSearchNews(searchTitle: searchTitle) { [weak self] newsData in
+            guard let self = self else { return }
+            self.newsList = newsData
+        }
+    }
+}
