06aefee75fd87e6fd7ae383850ca5bf24ee62851
Luna828 <eskw0701@naver.com>
2023-08-31 21:49:47 +0900

    [FEAT]: 페이지네이션 구현 -> 검색부분의 이슈

--- MobydickNews/Scenes/NewsSearch/ViewModel/NewsSearchViewModel.swift
+++ MobydickNews/Scenes/NewsSearch/ViewModel/NewsSearchViewModel.swift
@@ -12,32 +12,58 @@ import RxSwift
 final class NewsSearchViewModel: ObservableObject {
     private let newsManager = NewsApiDataManager.shared
     var newsList: NewsData?
-    // Subject - 이벤트를 발생 시키면서 Observable 형태도 되는 것
-    let newsListSubject = PublishSubject<Void>()
+    //
+    var articles: [Article] = []
+    var requestPage: Int = 0
+    //
+    let newsListSubject = PublishSubject<Bool>()
     private let disposeBag = DisposeBag()
 
-    func getAllHeadLineNews() {
+    func getAllHeadLineNews(isNeededToReset: Bool = false, page: Int) {
         newsManager.getAllHeadLineNews()?
             .bind(onNext: { [weak self] newsData in
                 guard let self = self else { return }
-                self.newsList = newsData
-                newsListSubject.onNext(())
+                if let articles = newsData.articles {
+                    self.articles += filteredArticle(articles: articles)
+                    self.requestPage += 1
+                }
+                newsListSubject.onNext(isNeededToReset)
             }).disposed(by: disposeBag)
     }
 
-    func getSearchNewsData(searchTitle: String) {
+//    func getSearchNewsData(isNeededToReset: Bool = false, searchTitle: String, page: Int) {
+//        newsManager.getSearchNews(searchTitle: searchTitle)?
+//            .subscribe(onNext: { [weak self] newsData in
+//                guard let self = self else { return }
+//                if let articles = newsData.articles {
+//                    let filteredArticles = self.filteredArticle(articles: articles)
+//                    if isNeededToReset {
+//                        self.articles = filteredArticles
+//                    } else {
+//                        self.articles += filteredArticles
+//                    }
+//                    self.requestPage += 1
+//                    self.newsListSubject.onNext(isNeededToReset)
+//                }
+//            }, onError: { error in
+//                print("Error fetching search news: \(error)")
+//            })
+//            .disposed(by: disposeBag)
+//    }
+
+    func getSearchNewsData(isNeededToReset: Bool = false, searchTitle: String, page: Int) {
         newsManager.getSearchNews(searchTitle: searchTitle)?
             .bind(onNext: { [weak self] newsData in
                 guard let self = self else { return }
-                self.newsList = newsData
-                newsListSubject.onNext(())
+                if let articles = newsData.articles {
+                    self.articles += filteredArticle(articles: articles)
+                    self.requestPage += 1
+                }
+                newsListSubject.onNext(isNeededToReset)
             }).disposed(by: disposeBag)
     }
 
-    func filteredArticle() -> [Article]? {
-        guard let newsList = newsList,
-              let articles = newsList.articles else { return nil }
-
+    func filteredArticle(articles: [Article]) -> [Article] {
         return articles.compactMap { article in
             if article.title != nil && article.content != nil { return article }
             return nil
